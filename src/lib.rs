9deadafce2247727900735e3f4315921c71dec05
Wes Kelly <weskelly@mac.com>
2024-10-31 07:32:32 -0700

    Add beginnings of SM builder

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,86 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+
+enum TransitionType<TEvent: Copy + Clone + Eq> {
+    OnEnter,
+    OnEvent(TEvent),
+    OnExit,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct StateMachineBuilder<
+    TState: Copy + Clone + Eq,
+    TEvent: Copy + Clone + Eq,
+    TGlobalState: Send + Sync = (),
+> {
+    working_with_state: TState,
+    current_machine: StateMachine<TState, TEvent, TGlobalState>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct StateMachine<
+    TState: Copy + Clone + Eq,
+    TEvent: Copy + Clone + Eq,
+    TGlobalState: Send + Sync = (),
+> {
+    current_state: TState,
+    global_state: Option<TGlobalState>,
+
+    handlers: HashMap<
+        (TState, TEvent, TransitionType<TEvent>),
+        Vec<fn(TGlobalState) -> anyhow::Result<()>>,
+    >,
+    transitions: HashMap<(TState, TEvent), TState>,
+}
+
+impl<TState: Copy + Clone + Eq, TEvent: Copy + Clone + Eq, TGlobalState: Send + Sync>
+    StateMachineBuilder<TState, TEvent, TGlobalState>
+{
+    pub fn create(initial_state: TState) -> Self {
+        Self {
+            working_with_state: initial_state,
+            current_machine: StateMachine::create(initial_state),
+        }
+    }
+
+    pub fn on_enter(self, action: fn(TGlobalState) -> anyhow::Result<()>) {
+        unimplemented!()
+    }
+}
+
+impl<TState: Copy + Clone + Eq, TEvent: Copy + Clone + Eq, TGlobalState: Send + Sync>
+    StateMachine<TState, TEvent, TGlobalState>
+{
+    fn create(initial_state: TState) -> Self {
+        Self {
+            current_state: initial_state,
+            global_state: None,
+            handlers: HashMap::new(),
+            transitions: HashMap::new(),
+        }
+    }
+
+    fn with_global_state(self, global_state: TGlobalState) -> Self {
+        Self {
+            global_state: Some(global_state),
+            ..self
+        }
+    }
+
+    fn in_state(self, state: TState) -> Self {
+        todo!("move to builder")
+    }
+
+    fn on_enter(self, state: TState, action: fn(TGlobalState) -> anyhow::Result<()>) -> Self {
+        todo!("move to builder")
+    }
+
+    fn on_exit(self, state: TState, action: fn(TGlobalState) -> anyhow::Result<()>) -> Self {
+        todo!("move to builder")
+    }
+
+    fn on(self, event: TEvent, in_state: TState, goto: TState) -> Self {
+        todo!("move to builder")
+    }
+
+    fn fire(&mut self, event: TEvent) -> anyhow::Result<()> {
+        todo!("move to builder")
     }
 }
